b3d403900a9f5fd30c2cfc59c276348af8e48ecb
Zeeshan Patel <89648030+DANYALZEE0@users.noreply.github.com>
2023-11-25 00:10:53 +0530

    Add files via upload

---
+++ 01_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; //  treat all js code as newer version/js
+
+/*alert("Hello") // we are using nodejs not Browser
+alert(3+3)*/
+
+console.log(3
+    +
+    3); // code readability should be High
+
+console.log("Zeeshan"); 
+
+
+let name = "Zeeshan"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// *Primitive Datatype
+// number => 2 to power 53.
+//bigint
+// BasicDT => "String"
+// boolean => true/false
+//null => Stand alone value 
+// undefined =>
+// symbol => about unique
+
+// *NonPrimitive Datatype
+//object 
+
+console.log(typeof "zeeshan");
+console.log(typeof age);
+console.log(typeof null); // type output : object
+console.log(typeof undefined); //undefined
+
+
