21488118b22fc81836c60145dd2284009336cd8e
Zeeshan Patel <89648030+DANYALZEE0@users.noreply.github.com>
2023-11-26 02:08:35 +0530

    Add files via upload

---
+++ 03_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+/// note = We Avoid this type of conparison in js it puts in confusion.
+
+// strict check
+// ===
+
+console.log("2" === 2);
+console.log("2" === "2");
