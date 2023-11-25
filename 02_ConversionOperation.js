1ce8500ea7f127e1c0db89996c49b413271dfdef
Zeeshan Patel <89648030+DANYALZEE0@users.noreply.github.com>
2023-11-26 00:40:32 +0530

    Add files via upload

---
+++ 02_ConversionOperation.js
@@ -0,0 +1,78 @@
+let score = "33abc"
+let score1 = null
+let score2 = undefined
+let score3 = true
+let score4 = "Zeeshan"
+let score5 = "33"
+
+console.log(typeof score);
+
+
+let valueInNumber = Number(score5)
+// let valueInNumber = int(score)   : This Will not work
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "3233" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Zeeshan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true;  0=> false
+// "" => false
+// "Zeeshan" => True
+
+let someNumber = 89
+
+let StringInsomeNumber = String(someNumber)
+console.log(StringInsomeNumber);
+console.log(typeof StringInsomeNumber);
+
+// *************************** Operatipons *******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue); 
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2); */
+
+// let str1 = "Zeeshan"
+// let str2 = " Patel"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// let number = "4"
+// let ConvNumber = Number(number)
+// let number1 = 6
+// console.log(number + number1);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+ 2 +2);
+// console.log((3+4) * 5 % 3);
+
+console.log(+"");
+console.log(+true);
+
+let num1 , num2 ,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
